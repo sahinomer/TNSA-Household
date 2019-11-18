3f71fcde313b55f45bb04456f4ac6d2b42f00486
sahinomer <sahin.omer@outlook.com>
2019-11-18 21:43:02 +0300

    Data preprocess

--- analyze_data.R
+++ analyze_data.R
@@ -1,17 +1,33 @@
 library(readr)
 library(tidyr)
+library(ggplot2)
 
-household <- read_delim("household.csv",";", escape_double = FALSE, trim_ws = TRUE, na = '')
+household <- read.table(file="household.csv", sep=";", header = TRUE, na.strings = c("" , " ","\t ", "Missing" ))
+
 household <- separate(household, region_combined, c("compass", "region", "settlement"))
-
-household$wealth_index <- factor(household$wealth_index)
 
 household <- na.omit(household)
 
-table(household$wealth_index)
+# Refactor levels of wealth index
+household$wealth_index <- factor(household$wealth_index, 
+                                 levels = c("Poorest", "Poorer", "Middle", "Richer", "Richest"))
 
-barplot(table(household$wealth_index), main="Wealth Distribution",
-        xlab="Number of Households")
+# Rate of related attributes
+household$man_member_rate = (household$household_member - household$woman_member) / household$household_member
+household$woman_member_rate = household$woman_member / household$household_member
+household$child_member_rate = household$children_under_5 / household$household_member
+household$bedroom_rate = household$bedroom_number / household$rooms_number
+
+
+ggplot(household, aes(x = wealth_index)) +
+  geom_bar() + ggtitle("Wealth Distribution") +
+  xlab("Wealth Index") + ylab("Number of Households")
+
+ggplot(household, aes(x = refrigerator, 
+                 y = owns_another_house,
+                 color = wealth_index)) + 
+  geom_point()
+
 
 table(household$wealth_index, household$house_ownership)
 table(household$wealth_index, household$owns_another_house)
