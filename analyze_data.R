7814c5560dddd4e5dd2ca7c9d449f936346bd6bc
sahinomer <sahin.omer@outlook.com>
2020-01-01 13:51:23 +0300

    Update analyze_data.R

--- analyze_data.R
+++ analyze_data.R
@@ -3,21 +3,29 @@ library(tidyr)
 library(ggplot2)
 library(DMwR2)
 
+###########################################################################
 
+# Load household data
 household <- read.table(file="household.csv", sep=";", header = TRUE, na.strings = c("" , " ","\t ", "Missing" ))
-
-household <- na.omit(household)
-
 household <- household[, -1]  # drop case id
 
-household <- separate(household, region_combined, c("compass", "region", "settlement"), sep = '-')
+# Unknown values
+sum(!complete.cases(household))
+household <- na.omit(household)
 
 ###########################################################################
+
+# Seperate and factirize region
+household <- separate(household, region_combined, c("cardinal_direction", "region", "settlement"), sep = '-')
+household$cardinal_direction <- as.factor(household$cardinal_direction)
+household$region <- as.factor(household$region)
+household$settlement <- as.factor(household$settlement)
 
 # Refactor levels of wealth index
 household$wealth_index <- factor(household$wealth_index, 
                                  levels = c("Poorest", "Poorer", "Middle", "Richer", "Richest"))
 
+###########################################################################
 
 # Rate of related attributes
 household$man_member_rate = (household$household_member - household$woman_member) / household$household_member
@@ -30,6 +38,8 @@ household <- household[ , -which(names(household) %in% c("woman_member",  # woma
                                                          "children_under_5", # children_member_rate
                                                          "bedroom_number"))]  # bedroom_rate
 
+summary(household)
+
 ###########################################################################
 
 # Plot distribution of data
@@ -41,7 +51,8 @@ for(colnm in colnames(household)) {
 ###########################################################################
 
 # Split train, validation and test set
-samples <- sample(1:nrow(household), nrow(household)*0.1)
+set.seed(1024)
+samples <- sample(1:nrow(household), nrow(household)*0.2)
 train <- household[samples, ]
 test <- household[-samples, ]
 
