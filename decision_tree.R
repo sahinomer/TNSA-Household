85a2abcd35d7a360b8aa1015d6bb05003e32422f
sahinomer <sahin.omer@outlook.com>
2020-01-17 15:29:18 +0300

    Update decision tree, create performance estimation

--- decision_tree.R
+++ decision_tree.R
@@ -22,7 +22,7 @@ decision_tree <- function(form, train, test, inf_gain, prune_se) {
 perfEst <- performanceEstimation(
           PredTask(wealth_index ~ ., train),
           workflowVariants(wf = "decision_tree", 
-                           inf_gain = c(0.20, 0.30, 0.40, 0.50), 
+                           inf_gain = c(0.1, 0.2, 0.3), 
                            prune_se = c(0.0, 0.2, 0.4, 0.6, 0.8, 1.0)),
           EstimationTask(metrics = "acc",
                          method = CV(nFolds = 5, seed = 1234)))
@@ -34,13 +34,17 @@ save(list=c("perfEst"), file = "decisiontree.perf")
 topPerformers(perfEst, maxs = TRUE)
 work_flow <- getWorkflow(topPerformers(perfEst, maxs = TRUE)[[1]][1,1], perfEst)
 
-inf_gain <- work_flow@pars[["learner.pars"]][["inf_gain"]]
-prune_se <- work_flow@pars[["learner.pars"]][["prune_se"]]
+inf_gain <- work_flow@pars[["inf_gain"]]
+prune_se <- work_flow@pars[["prune_se"]]
 
 # Test
-true_pred = decision_tree(wealth_index ~ ., train, test, inf_gain, prune_se)
-true_pred <- as_tibble(true_pred)
-test_conf_matrix <- table(true_pred$preds, true_pred$trues)
+information_gain <- attrEval(wealth_index ~ ., train, estimator = "InfGain")
+features <- names(information_gain)[information_gain>inf_gain]
+
+model <- rpartXse(wealth_index ~ ., train[,c("wealth_index", features)], se = prune_se)
+predicted <- predict(model, test, type = "class")
+
+test_conf_matrix <- table(predicted, test$wealth_index)
 test_conf_matrix
 test_accuracy <- sum(diag(test_conf_matrix)) / sum(test_conf_matrix)
 test_accuracy
